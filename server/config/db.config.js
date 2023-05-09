c27d6a2f0073a1db158db095425a9cfd53aa6263
mauricio5599 <codebuilders2000@gmail.com>
2023-05-09 09:35:43 -0600

    first commit

---
+++ server/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url: "mongodb+srv://mauricioportillo1891:qdpTs3oaJ2SzE9Os@cluster0.k0zu7o3.mongodb.net/?retryWrites=true&w=majority"
+};
+// ORMrehymjzoRmk9AUIbJIqG7Be3OvWDxjVSwPI9mGsyIBCOPgGNq8TD1yZhRMyQw
